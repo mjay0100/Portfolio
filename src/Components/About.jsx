b43123a85d5088f7943d87e01f86d142ba266a9c
mustapha jay <mustaphajay2@gmail.com>
2022-06-08 00:37:18 +0100

    initial commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+        <h2>Who I am</h2>
+        <p></p>
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <img src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3Aa2b6ebc9-cb5e-40d0-9e86-1576e9b3efeb&params=version%3A0&token=1654693586_da39a3ee_c9ed678403a2ff4343ec074fd245e5833e497c5b&api_key=CometServer1" alt="" />
+    </section>
+  )
+}
+
+export default About
