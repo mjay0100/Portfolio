dc7b72585e472f8733cf6c9ac0b306c7c35d004f
mustapha jay <mustaphajay2@gmail.com>
2022-06-08 23:17:05 +0100

    added navbar content

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,20 @@
 const Navbar = () => {
-    return ( 
-        <h1>navbar</h1>
-     );
-}
- 
+  return (
+    <header>
+      <div>
+        <img src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654693586_da39a3ee_c9ed678403a2ff4343ec074fd245e5833e497c5b&api_key=CometServer1" alt="" />
+      </div>
+      <div className="hamburger">
+        <span></span>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>My Services</li>
+        <li>About Me</li>
+        <li>My Work</li>
+      </ul>
+    </header>
+  );
+};
+
 export default Navbar;
