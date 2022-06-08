4ba25260272a1737642c99ecc2b114d33f14fd8c
mustapha jay <mustaphajay2@gmail.com>
2022-06-08 23:20:15 +0100

    added content and tailwind styles used external css for grid layout

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <section>
-        <h2>Who I am</h2>
-        <p></p>
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-        <img src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3Aa2b6ebc9-cb5e-40d0-9e86-1576e9b3efeb&params=version%3A0&token=1654693586_da39a3ee_c9ed678403a2ff4343ec074fd245e5833e497c5b&api_key=CometServer1" alt="" />
+    <section className="about-me">
+      <h2 className="about-title text-4xl font-bold self-center">Who I am</h2>
+      <p className="about-subtitle text-xl bg-[#16E0BD] p-2 self-center capitalize">
+        A passionate Web Designer from kano state
+      </p>
+      <div className="about-text self-center">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+      <img
+        className="about-img"
+        src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654780151_da39a3ee_efbf15b96277bfaeec60e354c7ce4356c8de10b7&api_key=CometServer1"
+        alt=""
+      />
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Intro = () => {
   return (
-    <div>Intro</div>
-  )
-}
+    <section className="intro ">
+      <h1 className="title text-4xl ">
+        Hi, I am <strong className="block">MJay</strong>
+      </h1>
+      <p className="subtitle bg-[#16E0BD] p-2 rounded-sm font-xl font-semibold">
+        Front End DEV
+      </p>
 
-export default Intro
+      <img
+        className="img shadow-sm shadow-[#16E0BD]/60 "
+        src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654780151_da39a3ee_efbf15b96277bfaeec60e354c7ce4356c8de10b7&api_key=CometServer1"
+        alt=""
+      />
+    </section>
+    // <section className="p-4 sm:block md:flex justify-center items-center gap-3 ">
+    //   <div className="md:order-last grid grid-cols-1">
+    //     {" "}
+    //     <h1 className="text-3xl mb-2 title">
+    //       Hi, I am <strong className="block">MJay</strong>
+    //     </h1>
+    //     <p className="subtitle bg-[#16E0BD] mb-2 text-2xl grow p-2 rounded-sm ">
+    //       Front End DEv
+    //     </p>
+    //   </div>
+    //   <img
+    //     className="img sm:w-full  md:w-[200px]  "
+    //     src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654780151_da39a3ee_efbf15b96277bfaeec60e354c7ce4356c8de10b7&api_key=CometServer1"
+    //     alt=""
+    //   />
+    // </section>
+  );
+};
+
+export default Intro;
