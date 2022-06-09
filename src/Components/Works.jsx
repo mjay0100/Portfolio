6be00bf0d4227fa8793fe2c4e984fa4be75f6c41
mustapha jay <mustaphajay2@gmail.com>
2022-06-09 14:26:01 +0100

    initial commit

---
+++ src/Components/Works.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Work} from '../Data'
+
+const Works = () => {
+  return (
+    <section>
+      <h2>My works</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, veniam.
+      </p>
+      <div>
+        <a href="">
+          <img
+            src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654693586_da39a3ee_c9ed678403a2ff4343ec074fd245e5833e497c5b&api_key=CometServer1"
+            alt=""
+          />
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Works;
