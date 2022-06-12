a6b466d5ef1cb0403ffca57015e3f8eb4f503af0
mustapha jay <mustaphajay2@gmail.com>
2022-06-12 19:55:59 +0100

    added reat-router-dom and has-link for navigation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,42 @@
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    console.log("hi");
+  };
   return (
-    <header>
-      <div>
-        <img src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654693586_da39a3ee_c9ed678403a2ff4343ec074fd245e5833e497c5b&api_key=CometServer1" alt="" />
-      </div>
-      <div className="hamburger">
-        <span></span>
-      </div>
-      <ul>
-        <li>Home</li>
-        <li>My Services</li>
-        <li>About Me</li>
-        <li>My Work</li>
-      </ul>
-    </header>
+    <BrowserRouter>
+      <header className="flex justify-between px-[2rem] my-5 relative ">
+        <div>
+          <h1 className="text-2xl">MJ</h1>
+        </div>
+        <div
+          className="hamburger sm:absolute top-1 right-3 flex items-center md:hidden "
+          onClick={handleClick}
+        >
+          <FaBars className="text-2xl " />
+        </div>
+        <nav>
+          <ul className="hidden md:flex justify-around gap-3 text-lg  ">
+            <Link to="#home" smooth className="px-2">
+              Home
+            </Link>
+            <Link to="#services" smooth className="px-2">
+              Services
+            </Link>
+            <Link to="#about" smooth className="px-2">
+              About Me
+            </Link>
+            <Link to="#works" smooth className="px-2">
+              Works
+            </Link>
+          </ul>
+        </nav>
+      </header>
+    </BrowserRouter>
   );
 };
 
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-import {Service} from "../Data";
+import { Service } from "../Data";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 const Services = () => {
-  
   return (
-    <section className="services bg-no-repeat bg-cover text-white text-center  ">
-      <h1 className="text-3xl p-4 text-[#14CFAE]">What I do</h1>
-      <div className="w-[50px] h-[2px] mx-auto bg-[#14CFAE] opacity-50 text-center "></div>
-      <div className="md:flex">
-        {Service.map((item) => {
-          const { id, title, body } = item;
-          return (
-            <div key={id} className="p-5">
-              <h3>{title}</h3>
-              <p className="">{body}</p>
-            </div>
-          );
-        })}
-      </div>
-      <button className="btn bg-[#16E0BD] my-[2rem] p-2 rounded-sm text-black hover:scale-105 transition-transform  ">
-        see my work
-      </button>
-    </section>
+    <BrowserRouter>
+      <section
+        className="services bg-no-repeat bg-cover text-white text-center  "
+        id="services"
+      >
+        <h1 className="text-3xl p-4 text-[#14CFAE]">What I do</h1>
+        <div className="w-[50px] h-[2px] mx-auto bg-[#14CFAE] opacity-50 text-center "></div>
+        <div className="md:flex">
+          {Service.map((item) => {
+            const { id, title, body } = item;
+            return (
+              <div key={id} className="p-5">
+                <h3>{title}</h3>
+                <p className="">{body}</p>
+              </div>
+            );
+          })}
+        </div>
+        <Link
+          to="#works"
+          className="btn bg-[#16E0BD] my-[2rem] p-2 rounded-sm text-black hover:scale-105 transition-transform  "
+        >
+          see my work
+        </Link>
+      </section>
+    </BrowserRouter>
   );
 };
 
