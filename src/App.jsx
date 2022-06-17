9c9112e334bdc046834b19b800f3db63433beef0
mustapha jay <mustaphajay2@gmail.com>
2022-06-17 21:34:37 +0100

    added useEffect and initialised aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import Content from "./Components/Content";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+    Aos.refresh();
+  }, []);
   return (
     <div>
       <Navbar />
