a1114eeaf4008543aea7933a869bd30e01a1798c
mustapha jay <mustaphajay2@gmail.com>
2022-06-18 00:07:20 +0100

    imported images to work array

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,4 +1,9 @@
 import { FaGithub, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import colorGame from "./images/colorGame.png";
+import Iwd from "./images/Iwd.png";
+import list from "./images/list.png";
+import stripe from "./images/stripe.png";
+
 export const Service = [
   {
     id: 1,
@@ -7,6 +12,11 @@ export const Service = [
   },
   {
     id: 2,
+    title: "Design + Development",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 4,
     title: "Design + Development",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
@@ -20,23 +30,23 @@ export const Service = [
 export const Work = [
   {
     id: 1,
-    image:
-      "https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3Aea0eb191-b57d-4f1d-80c2-72a2b59bd819&params=version%3A0&token=1654865680_da39a3ee_3d5bb2a7b0a5b8e33cd94a167dd71cb0b39497c4&api_key=CometServer1",
+    url: "https://the-rgb-game.netlify.app/",
+    image: colorGame,
   },
   {
     id: 2,
-    image:
-      "https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3Aea0eb191-b57d-4f1d-80c2-72a2b59bd819&params=version%3A0&token=1654865680_da39a3ee_3d5bb2a7b0a5b8e33cd94a167dd71cb0b39497c4&api_key=CometServer1",
+    url: "https://the-grocery-list.netlify.app/",
+    image: list,
   },
   {
     id: 3,
-    image:
-      "https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3Aea0eb191-b57d-4f1d-80c2-72a2b59bd819&params=version%3A0&token=1654865680_da39a3ee_3d5bb2a7b0a5b8e33cd94a167dd71cb0b39497c4&api_key=CometServer1",
+    url: "https://iwdindia.in/",
+    image: Iwd,
   },
   {
     id: 4,
-    image:
-      "https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3Aea0eb191-b57d-4f1d-80c2-72a2b59bd819&params=version%3A0&token=1654865680_da39a3ee_3d5bb2a7b0a5b8e33cd94a167dd71cb0b39497c4&api_key=CometServer1",
+    url: "https://my-stripe-clone.netlify.app/",
+    image: stripe,
   },
 ];
 
