a38088f06be031781016383da5e3c7eeae934945
mustapha jay <mustaphajay2@gmail.com>
2022-06-18 00:13:16 +0100

    added animation and updated some css

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <section className="intro " id='home'>
-      <h1 className="title text-4xl ">
+    <section className="intro " id="home">
+      <h1
+        data-aos="fade-left"
+        data-aos-duration="2000"
+        data-aos-mirror="true"
+        className="title text-4xl "
+      >
         Hi, I am <strong className="block">MJay</strong>
       </h1>
       <p className="subtitle bg-[#16E0BD] p-2 rounded-sm font-xl font-semibold">
@@ -11,8 +16,11 @@ const Intro = () => {
       </p>
 
       <img
+        data-aos="fade-right"
+        data-aos-duration="2000"
+        data-aos-mirror="true"
         className="img shadow-sm shadow-[#16E0BD]/60 "
-        src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654780151_da39a3ee_efbf15b96277bfaeec60e354c7ce4356c8de10b7&api_key=CometServer1"
+        src="https://moglan.netlify.app/images/beautiful-curly-blonde-young-woman-white-trendy-blouse-touches-hair-attractive-girl-shirt-pearl-necklace-poses-outside.png"
         alt=""
       />
     </section>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -10,21 +10,31 @@ const Services = () => {
         className="services bg-no-repeat bg-cover text-white text-center  "
         id="services"
       >
-        <h1 className="text-3xl p-4 text-[#14CFAE]">What I do</h1>
+        <h1 className="text-3xl p-4 ">What I do</h1>
         <div className="w-[50px] h-[2px] mx-auto bg-[#14CFAE] opacity-50 text-center "></div>
-        <div className="md:flex">
+        <div className="md:grid grid-cols-2">
           {Service.map((item) => {
             const { id, title, body } = item;
             return (
-              <div key={id} className="p-5">
-                <h3>{title}</h3>
+              <div
+                data-aos="zoom-in-down"
+                data-aos-duration="2000"
+                data-aos-mirror="true"
+                key={id}
+                className="p-5"
+              >
+                <h3 className="text-2xl font-semibold mb-3 text-[#14CFAE]">
+                  {title}
+                </h3>
                 <p className="">{body}</p>
               </div>
             );
           })}
         </div>
         <button className="btn bg-[#16E0BD] my-[2rem] p-2 rounded-md text-black hover:scale-110 transition-all duration-300 ease-in-out ">
-          <Link to="#works">see my work</Link>
+          <Link to="#works" className="p-[1rem]">
+            see my work
+          </Link>
         </button>
       </section>
     </BrowserRouter>
