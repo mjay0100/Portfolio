b8d6ea9e2a93c85e061e5881225a6214bbc1a0a8
mustapha jay <mustaphajay2@gmail.com>
2022-06-18 00:15:40 +0100

    added animation

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-
 const About = () => {
   return (
     <section className="about-me" id="about">
-      <h2 className="about-title text-4xl font-bold self-center">Who I am</h2>
-      <p className="about-subtitle text-xl bg-[#16E0BD] p-2 self-center capitalize">
-        A passionate Web Designer from kano state
+      <h2
+        data-aos-duration="1500"
+        data-aos-mirror="true"
+        data-aos="fade-right"
+        className="about-title text-4xl font-bold self-center"
+      >
+        Who I am
+      </h2>
+      <p
+        data-aos-duration="1500"
+        data-aos-mirror="true"
+        data-aos="fade-right"
+        className="about-subtitle text-xl bg-[#16E0BD] p-2 self-center capitalize"
+      >
+        A passionate Web Designer
       </p>
-      <div className="about-text self-center">
+      <div
+        data-aos-duration="1500"
+        data-aos-mirror="true"
+        data-aos="fade-right"
+        className="about-text self-center"
+      >
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -22,8 +37,11 @@ const About = () => {
         </p>
       </div>
       <img
+        data-aos-duration="1500"
+        data-aos-mirror="true"
+        data-aos="fade-left"
         className="about-img"
-        src="https://public-v2links.adobecc.com/6ebfeb86-6eeb-4b69-77dc-ecf4c4506bcc/component?params=component_id%3A94e37e08-c26d-4c22-933c-be84b27328b2&params=version%3A0&token=1654780151_da39a3ee_efbf15b96277bfaeec60e354c7ce4356c8de10b7&api_key=CometServer1"
+        src="https://moglan.netlify.app/images/1lip.jpg"
         alt=""
       />
     </section>
