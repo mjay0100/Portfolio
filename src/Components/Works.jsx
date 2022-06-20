f64edec9bf3f98e7361a87708ec74fca2dce81e3
mustapha jay <mustaphajay2@gmail.com>
2022-06-20 21:45:57 +0100

    modified dummy text tomy info

--- src/Components/Works.jsx
+++ src/Components/Works.jsx
@@ -9,8 +9,7 @@ const Works = () => {
         <h2 className="text-white text-3xl mb-4">My works</h2>
 
         <p className="text-[#14CFAE] mb-[2rem]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis,
-          veniam.
+          A List of some of my works 😁😎.
         </p>
       </div>
       <div className="text-center w-fit mx-auto grid sm:grid-cols-2 lg:grid-cols-4 -[5rem] ">
@@ -21,7 +20,7 @@ const Works = () => {
               data-aos="zoom-in-up"
               data-aos-duration="2000"
               key={id}
-              className="border-2 border-solid border-[#14CFAE] overflow-hidden gmb-[1rem] md:mb-0"
+              className="border hover:border-2 border-solid border-[#14CFAE] overflow-hidden  md:mb-0"
             >
               <a href={url}>
                 <img
